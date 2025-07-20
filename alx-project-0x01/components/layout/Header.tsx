f6fe2065165549c2aac344478956a9a3aaf297d8
Hillary Robert <roberthillary30@gmail.com>
2025-07-20 15:24:32 +0300

    Initial commit for alx-project-0x01

---
+++ alx-project-0x01/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="p-4 bg-gray-100 shadow">
+      <h1 className="text-2xl font-bold">Site Header</h1>
+    </header>
+  );
+};
+
+export default Header;
